93a665e79f946e5749b4c9550179fe3117989243
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-06-03 23:59:10 -0400

    sets a timeout on page

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
-
+  const [count, setCount] = useState(0);
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1);
+    }, 1000);
 
+    if (timeRemaining === 0) {
+      clearTimeout(timer); // Clear the timeout when countdown reaches 0
+      onAnswered(false);
+    }
+
+    return () => {
+      clearTimeout(timer); // Cleanup: clear the timeout if the component unmounts
+    };
+  }, [timeRemaining]);
+  
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
